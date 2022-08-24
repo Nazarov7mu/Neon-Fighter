366ebf35131164306ecc7b795b30830d5124a93e
Javokhir Nazarov <nazarov7mu@gmail.com>
2022-08-24 13:44:11 +0500

    Project setup

---
+++ Assets/Scripts/GameSettings/FPS.cs
@@ -0,0 +1,29 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace GameSettings
+{
+    public class FPS : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _fpsText;
+    
+        private float _deltaTime;
+
+        private void Awake()
+        {
+            Application.targetFrameRate = 120;
+        }
+
+        private void Update()
+        {
+            if (Time.timeScale != 0) ShowFPS();
+        }
+
+        private void ShowFPS()
+        {
+            _deltaTime += (Time.deltaTime - _deltaTime) * 0.1f;
+            float fps = 1.0f / _deltaTime;
+            _fpsText.text = $"{Mathf.Ceil(fps)}";
+        }
+    }
+}
---
+++ Assets/Scripts/GameSettings/GameSpeed.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace GameSettings
+{
+    public class GameSpeed : MonoBehaviour
+    {
+        public void ResumeTime() => ChangeTimeScale(1);
+
+        public void StopTime() => ChangeTimeScale(0);
+
+        public void SetTime(float value) => ChangeTimeScale(value);
+
+        private void Awake()
+        {
+            ResumeTime();
+        }
+
+        private void ChangeTimeScale(float value) => Time.timeScale = value;
+    }
+}
